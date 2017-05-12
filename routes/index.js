9dbd3be825090dfe204ca6fcd2125c86ad5d5299
Andrzej Szmajnta <andrzejdaniel.szm@gmail.com>
2017-05-12 11:56:12 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,120 @@
+var express = require('express');
+var rp = require('request-promise');
+var items = require('./items');
+var router = express.Router();
+var async = require('async');
+
+const headers = {
+    'X-App-Id': '40d652b3-152f-40fb-8ec2-3fb8ee16b5cb',
+    'X-App-Token': 'a82abad0-2520-4a3c-bb85-c404f9f1a782'
+};
+const apiRoot = 'https://api.voucherify.io/v1/vouchers/';
+
+router.post('/referrer', function (req, res) {
+    rp({
+        uri: apiRoot + 'publish',
+        method: 'POST',
+        headers: headers,
+        body: {
+            campaign: 'Example referral campaign',
+            customer: {
+                source_id: req.body.email,
+                email: req.body.email,
+                name: req.body.fullname
+            }
+        },
+        json: true
+    }).then(function (resp) {
+        res.send(resp);
+    }).catch(function () {
+        res.status(400).send("Error");
+    });
+});
+
+router.get('/item', function (req, res) {
+    res.send(items.randomElement());
+});
+
+router.get('/verify', function (req, res) {
+    const code = req.query.code;
+    rp({
+        uri: apiRoot + code,
+        headers: headers,
+        json: true
+    }).then(function (resp) {
+        res.send({
+            status: resp.active,
+            discount: resp.discount
+        });
+    }).catch(function () {
+        res.status(400).send({status: false});
+    });
+});
+
+router.post('/finalize', function (req, res) {
+    const code = req.body.code;
+    const game = req.body.game;
+    const body = {
+        customer: {
+            name: "Test test",
+            email: "test@morgan.com"
+        },
+        order: {
+            amount: 1,
+            items: []
+        },
+        metadata: game
+    };
+    rp({
+        uri: apiRoot + code + '/redemption',
+        method: 'POST',
+        headers: headers,
+        body: body,
+        json: true
+    }).then(function (resp) {
+        res.send(resp);
+    }).catch(function () {
+        res.status(400).send("Error");
+    });
+});
+
+router.get('/getRedemptions', function (req, res) {
+    const code = req.query.code;
+    async.parallel({
+        data: function (call) {
+            rp({
+                uri: apiRoot + code,
+                headers: headers,
+                json: true
+            }).then(function (res1) {
+                call(null, res1.publish.entries[0]);
+            }).catch(function (err) {
+                call(err);
+            });
+        },
+        entries: function (call) {
+            rp({
+                uri: apiRoot + code + '/redemption',
+                headers: headers,
+                json: true
+            }).then(function (res2) {
+                call(null, res2)
+            }).catch(function (err) {
+                call(err);
+            });
+        }
+    }, function (err, results) {
+        if (err) res.status(400).send({status: false});
+        res.send({
+            customer: results.data,
+            entries: results.entries.redemption_entries,
+            total: results.entries.total
+        });
+    });
+});
+
+Array.prototype.randomElement = function () {
+    return this[Math.floor(Math.random() * this.length)]
+};
+
+module.exports = router;
---
+++ routes/items.js
@@ -0,0 +1,46 @@
+/**
+ * Created by andrzejszmajnta on 10.05.2017.
+ */
+module.exports = [
+    {
+        id: 1,
+        name: 'Pandemic Legacy: Season 1',
+        sec_name: '',
+        picture_url: 'http://cf.geekdo-images.com/images/pic2452831_md.png',
+        description: 'Pandemic Legacy is a co-operative campaign game, with an overarching story-arc played through 12-24 sessions, depending on how well your group does at the game. At the beginning, the game starts very similar to basic Pandemic, in which your team of disease-fighting specialists races against the clock to travel around the world, treating disease hotspots while researching cures for each of four plagues before they get out of hand.',
+        price: 50.99,
+        currency: '$'
+    },
+    {
+        id: 2,
+        name: 'Through the Ages: A New Story of Civilization',
+        picture_url: 'http://cf.geekdo-images.com/32e-PrFMZ0-P_KsnZHApZazlPqc=/fit-in/246x300/pic2663291.jpg',
+        description: "Through the Ages: A New Story of Civilization is the new edition of Through the Ages: A Story of Civilization, with many changes small and large to the game's cards over its three ages and extensive changes to how military works Through the Ages is a civilization building game. Each player attempts to build the best civilization through careful resource management, discovering new technologies, electing the right leaders, building wonders and maintaining a strong military. Weakness in any area can be exploited by your opponents. The game takes place throughout the ages beginning in the age of antiquity and ending in the modern age.",
+        price: 62.99,
+        currency: '$'
+    },
+    {
+        id: 3,
+        name: 'Twilight Struggle',
+        picture_url: 'http://cf.geekdo-images.com/0_gpX_v9CeKcm60nSkDzi47PVOA=/fit-in/246x300/pic361592.jpg',
+        description: "Twilight Struggle inherits its fundamental systems from the card-driven classics We the People and Hannibal: Rome vs. Carthage. It is a quick-playing, low-complexity game in that tradition. The game map is a world map of the period, whereon players move units and exert influence in attempts to gain allies and control for their superpower. As with GMT's other card-driven games, decision-making is a challenge; how to best use one's cards and units given consistently limited resources?",
+        price: 23.99,
+        currency: '$'
+    },
+    {
+        id: 4,
+        name: 'Terra Mystica',
+        picture_url: 'http://cf.geekdo-images.com/zm6mWUEYg2Yco_sJpjmN1DBZZv8=/fit-in/246x300/pic1356616.jpg',
+        description: "Terra Mystica is a game with very little luck that rewards strategic planning. Each player governs one of the 14 groups. With subtlety and craft, the player must attempt to rule as great an area as possible and to develop that group's skills. There are also four religious cults in which you can progress. To do all that, each group has special skills and abilities.",
+        price: 37.99,
+        currency: '$'
+    },
+    {
+        id: 5,
+        name: 'Star Wars: Rebellion',
+        picture_url: 'http://cf.geekdo-images.com/-g4rv4QjPCPg4M4s2COe3AMZFrs=/fit-in/246x300/pic2737530.png',
+        description: "Star Wars: Rebellion is a board game of epic conflict between the Galactic Empire and Rebel Alliance for two to four players! Experience the Galactic Civil War like never before. In Rebellion, you control the entire Galactic Empire or the fledgling Rebel Alliance. You must command starships, account for troop movements, and rally systems to your cause.",
+        price: 90.49,
+        currency: '$'
+    },
+];
